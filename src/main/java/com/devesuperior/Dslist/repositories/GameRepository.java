581e39a902c4f59368ba8ffd74c69274d5049bdd
maxtrunder <maxtrunder@hotmail.com>
2024-11-13 23:53:50 -0300

    Game repsitory, service, controller

---
+++ src/main/java/com/devesuperior/Dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.devesuperior.Dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devesuperior.Dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game,Long>{
+
+}
